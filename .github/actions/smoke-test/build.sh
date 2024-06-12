533429fc8ccbbc69773edf4a9d86a7e49595e1ab
Rene B. Pinto <renebentes@yahoo.com.br>
2024-06-12 11:31:21 -0300

    feat: define a prebuilt devcontainer
    The prebuilt image will be used to define the devcontainer templates so
    that the workflow can be streamlined and does not have to be built for
    each use.
    
    A prebuilt image does not require the devcontainer-template.json file
    during smoke testing, so we validate its existence during the build.

--- .github/actions/smoke-test/build.sh
+++ .github/actions/smoke-test/build.sh
@@ -18,33 +18,35 @@ fi
 SRC_DIR="/tmp/${TEMPLATE_ID}"
 cp -R "src/${TEMPLATE_ID}" "${SRC_DIR}"
 
-pushd "${SRC_DIR}"
+if [ -f "${SRC_DIR}/devcontainer-template.json" ]; then
+    pushd "${SRC_DIR}"
 
-# Configure templates only if `devcontainer-template.json` contains the `options` property.
-OPTION_PROPERTY=($(jq -r '.options' devcontainer-template.json))
+    # Configure templates only if `devcontainer-template.json` contains the `options` property.
+    OPTION_PROPERTY=($(jq -r '.options' devcontainer-template.json))
 
-if [ "${OPTION_PROPERTY}" != "" ] && [ "${OPTION_PROPERTY}" != "null" ]; then
-    OPTIONS=($(jq -r '.options | keys[]' devcontainer-template.json))
+    if [ "${OPTION_PROPERTY}" != "" ] && [ "${OPTION_PROPERTY}" != "null" ]; then
+        OPTIONS=($(jq -r '.options | keys[]' devcontainer-template.json))
 
-    if [ "${OPTIONS[0]}" != "" ] && [ "${OPTIONS[0]}" != "null" ]; then
-        echo "(!) Configuring template options for '${TEMPLATE_ID}'"
-        for OPTION in "${OPTIONS[@]}"; do
-            OPTION_KEY="\${templateOption:$OPTION}"
-            OPTION_VALUE=$(jq -r ".options | .${OPTION} | .default" devcontainer-template.json)
+        if [ "${OPTIONS[0]}" != "" ] && [ "${OPTIONS[0]}" != "null" ]; then
+            echo "(!) Configuring template options for '${TEMPLATE_ID}'"
+            for OPTION in "${OPTIONS[@]}"; do
+                OPTION_KEY="\${templateOption:$OPTION}"
+                OPTION_VALUE=$(jq -r ".options | .${OPTION} | .default" devcontainer-template.json)
 
-            if [ "${OPTION_VALUE}" = "" ] || [ "${OPTION_VALUE}" = "null" ]; then
-                echo "Template '${TEMPLATE_ID}' is missing a default value for option '${OPTION}'"
-                exit 1
-            fi
+                if [ "${OPTION_VALUE}" = "" ] || [ "${OPTION_VALUE}" = "null" ]; then
+                    echo "Template '${TEMPLATE_ID}' is missing a default value for option '${OPTION}'"
+                    exit 1
+                fi
 
-            echo "(!) Replacing '${OPTION_KEY}' with '${OPTION_VALUE}'"
-            OPTION_VALUE_ESCAPED=$(sed -e 's/[]\/$*.^[]/\\&/g' <<<"${OPTION_VALUE}")
-            find ./ -type f -print0 | xargs -0 sed -i "s/${OPTION_KEY}/${OPTION_VALUE_ESCAPED}/g"
-        done
+                echo "(!) Replacing '${OPTION_KEY}' with '${OPTION_VALUE}'"
+                OPTION_VALUE_ESCAPED=$(sed -e 's/[]\/$*.^[]/\\&/g' <<<"${OPTION_VALUE}")
+                find ./ -type f -print0 | xargs -0 sed -i "s/${OPTION_KEY}/${OPTION_VALUE_ESCAPED}/g"
+            done
+        fi
     fi
-fi
 
-popd
+    popd
+fi
 
 if [ ! -d "test/${TEMPLATE_ID}" ]; then
     echo "Template '${TEMPLATE_ID}' is missing a test folder"
